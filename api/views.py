5db05f3dcf3f9492e44d9357e0d0146a94f6d11e
Diogo Laginha <dmachado@ipn.pt>
2011-12-31 16:39:00 +0000

    add support to create new wine

--- api/views.py
+++ api/views.py
@@ -3,6 +3,12 @@
 from django.http import HttpResponse
 from api.utils   import Resource, method
 from api.models  import Wine
+import ast
+
+def query_to_dict( x ):
+    x = str( x.items()[0][0] )
+    x = x.replace( 'null', 'None' ) 
+    return ast.literal_eval( x )
 
 class WinesResource(Resource):
 
@@ -10,9 +16,17 @@ class WinesResource(Resource):
     def GET(request):
         return Wine.objects.all()
 
+    @method
+    def POST(request, *args, **kwargs):
+        fields = query_to_dict( request.POST )
+        wine   = Wine( **fields )
+        wine.save()
+        return wine
+        
+
 class WineResource(Resource):
 
     @method
     def GET(request, wine_id):
         return Wine.objects.get( id=int(wine_id) )
-
+    
